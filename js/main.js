f6952cdc7aea5ff6ff99e2271d16bad9ababb523
Toni Tesori <tt@tonitesori.dev>
2022-05-27 13:30:27 -0400

    Basic functionality

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@ const fetchNum = async () => {
   try {
     const resp = await fetch('/api');
     const json = await resp.json();
-    console.log(json);
+    return json.winner;
   } catch (error) {
     console.error(error);
   }
 
 };
 
-document.querySelector('#btn-click').addEventListener('click', fetchNum);
+const chooseWinner = async () => {
+  const resultEl = document.querySelector('.txt-result');
+  const winner = await fetchNum();
+  resultEl.textContent = 'Generating winner...';
+  setTimeout(() => {
+    resultEl.textContent = `The winner is ${winner}`;
+  }, 500);
+
+};
+
+document.querySelector('#btn-click').addEventListener('click', chooseWinner);
